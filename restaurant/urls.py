48e063bf9679e9c86b30b83f30c009a5aa31fa55
Moussa Hassana <moussahassana42@gmail.com>
2024-12-17 11:41:56 +0100

    feat: add URL paths for Booking

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'), 
     path('menu/', views.MenuItemView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('bookings/', views.BookingViewSet.as_view({'get': 'list', 'post': 'create'}), name='booking-list'),
+    path('bookings/<int:pk>', views.BookingViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='booking-detail'),
     path('api-token-auth/', obtain_auth_token),
 ]
